7b88607891c36943be041e7502937b0025efc3ac
AnshikaVerma38 <vermaanshika6821@gmail.com>
2025-05-04 01:20:04 +0530

    day3

---
+++ day3.java
@@ -0,0 +1,21 @@
+public class day3 {
+    public static void reverse(int[] arr) {
+        int i = 0, j = arr.length - 1;
+        while (i<= j) {
+            // Swap arr[left] and arr[right]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
